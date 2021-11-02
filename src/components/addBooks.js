9f926f60f70bb57fde46b95d328ba144649764a9
Amalcxc <amalcxirsi@gmail.com>
2021-11-02 22:01:08 +0300

    input for the author

--- src/components/addBooks.js
+++ src/components/addBooks.js
@@ -1,6 +1,7 @@
 const AddBook = () => (
   <form>
     <input type="text" placeholder="Book title" />
+    <input type="text" placeholder="Book Author" />
     <select disabled>
       <option value="category">Category</option>
     </select>
