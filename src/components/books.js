e25ba1468284987dd0fcab6a9d7ae88254aaeb5f
Amalcxc <amalcxirsi@gmail.com>
2021-11-05 19:19:09 +0300

    project styles finish

--- src/components/books.js
+++ src/components/books.js
@@ -15,20 +15,27 @@ const Books = (prop) => {
   };
 
   return <div className='Lesson-Panel'>
-    <span>{category}</span>
-    <span>{title}</span>
-    <span>{author}</span>
-    <div className='books-buttons'>
-      <button className='Comments'>Comments</button>
-      <button className='Remove' onClick={removeHandler} type="button">Remove</button>
-      <button className='Edit'>Edit</button>
-    </div>
     <div>
-     <span>{completed}</span>
+      <span className='Categories'>{category}</span>
+      <h2 className='Title'>{title}</h2>
+      <h6 className='Author'>{author}</h6>
+      <div className='books-buttons'>
+        <button className='Comments'>Comments</button>
+        <button className='Remove' onClick={removeHandler} type="button">Remove</button>
+        <button className='Edit'>Edit</button>
+      </div>
     </div>
-    <div>
-    <span className="Current-Chapter">Current Chapter</span>
-    <span>{chapter}</span>
+    <div className='Percent-c'>
+     <div class="Oval-2"></div>
+     <div>
+      <h3 className='Percent-Complete'>{completed}%</h3>
+      <h3 className='Completed'>completed</h3>
+     </div>
+
+    </div>
+    <div className='chapter'>
+    <h3 className="Current-Chapter">Current Chapter</h3>
+    <h3 className='Current-Lesson'>Chapter {chapter}</h3>
     <button className='Update-progress'>Update progress</button>
     </div>
   </div>;
